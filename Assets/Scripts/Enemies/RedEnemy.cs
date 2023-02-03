d5ce138f177efcd52d4e21b6f57eefb014232571
AlexBraginets <qetting@gmail.com>
2023-02-03 15:32:19 +0200

    Update RedEnemy.cs

--- Assets/Scripts/Enemies/RedEnemy.cs
+++ Assets/Scripts/Enemies/RedEnemy.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections;
+using System.Runtime.InteropServices;
+using DG.Tweening;
 using Enemies.Data;
 using UnityEngine;
 
@@ -7,8 +9,13 @@ namespace Enemies
 {
     public class RedEnemy : Enemy
     {
+        private void Awake()
+        {
+            Attack(_player);
+        }
+
         [SerializeField] private RedEnemyData config;
-        private Transform _player;
+        [SerializeField] private Transform _player;
         private void Setup(Transform player)
         {
             _player = player;
@@ -17,12 +24,41 @@ namespace Enemies
         public void Attack(Transform player)
         {
             Setup(player);
-            
+            StartCoroutine(AttackCorout());
         }
 
+        private IEnumerator AttackCorout()
+        {
+            yield return FlyAbove();
+            yield return Pause();
+            yield return ChasePlayer();
+        }
+
+        private IEnumerator Pause()
+        {
+            yield return new WaitForSeconds(config.PauseDuration);
+        }
+
+        private IEnumerator ChasePlayer()
+        {
+            float simulaitonStep = .025f;
+            WaitForSeconds wait = new WaitForSeconds(simulaitonStep);
+            while (true)
+            {
+
+                yield return wait;
+                Vector3 direction = (_player.position - transform.position).normalized;
+                transform.position += direction * config.AttackSpeed * simulaitonStep;
+            }
+        }
         private IEnumerator FlyAbove()
         {
-            throw new NotImplementedException();
+            float yOffset = config.FlyAboveOffset;
+            float duration = config.FlyAboveDuration;
+            float y = transform.position.y;
+            float yEnd = y + yOffset;
+            transform.DOMoveY(yEnd, duration);
+            yield return new WaitForSeconds(duration);
         }
     }
 }
