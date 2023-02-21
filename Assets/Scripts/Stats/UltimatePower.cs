5c6be388f872db2724876a8505ff17d17e383849
AlexBraginets <qetting@gmail.com>
2023-02-21 13:44:19 +0200

    Ultimate Power

--- Assets/Scripts/Stats/UltimatePower.cs
+++ Assets/Scripts/Stats/UltimatePower.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class UltimatePower : MonoBehaviour
+namespace Stats
 {
-    // Start is called before the first frame update
-    void Start()
+    public class UltimatePower : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private float initialPower;
+        [SerializeField] private float maxPower;
+        public float RelativePower => initialPower / maxPower;
+        public event Action<float> OnChanged;
+        public event Action<float> OnMaxPower;
+        private float _value;
+        private const float TOLERANCE = Single.Epsilon * 10f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            Value = initialPower;
+        }
+
+        public float Value
+        {
+            get => _value;
+            set
+            {
+                if (Mathf.Abs(value - _value) < TOLERANCE) return;
+                _value = Mathf.Clamp(value, 0f, maxPower);
+                OnChanged?.Invoke(_value);
+                if (Mathf.Abs(_value - maxPower) < TOLERANCE)
+                {
+                    OnMaxPower?.Invoke(maxPower);
+                }
+            }
+        }
     }
 }
